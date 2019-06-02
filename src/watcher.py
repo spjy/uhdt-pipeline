db4fd3b477669854fc3473e80d0e0a70d504926a
spjy <spjy@hawaii.edu>
2019-06-02 09:55:49 -1000

    style: adjust tab spacing

--- src/watcher.py
+++ src/watcher.py
@@ -6,47 +6,46 @@ from watchdog.events import FileSystemEventHandler
 from simple_rest_client.api import API
 
 class Watcher:
-    DIRECTORY_TO_WATCH = "C:\watcher_directory"
+  DIRECTORY_TO_WATCH = "C:\watcher_directory"
 
-    def __init__(self):
-        self.observer = Observer()
+  def __init__(self):
+    self.observer = Observer()
 
-    def run(self):
-        event_handler = Handler()
-        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
-        self.observer.start()
-        try:
-            while True:
-                time.sleep(5)
-        except:
-            self.observer.stop()
-            print("Error")
+  def run(self):
+    event_handler = Handler()
+    self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+    self.observer.start()
+    try:
+      while True:
+          time.sleep(5)
+    except:
+      self.observer.stop()
+      print("Error")
 
-        self.observer.join()
+    self.observer.join()
 
 
 class Handler(FileSystemEventHandler):
+  @staticmethod
+  def on_any_event(event):
+    if event.is_directory:
+      return None
 
-    @staticmethod
-    def on_any_event(event):
-        if event.is_directory:
-            return None
+    elif event.event_type == 'created':
+      # Take any action here when a file is first created.
+      print("Received created event - %s." % event.src_path)
+      filepath = event.src_path.split(os.sep)
+      sep = os.sep
+      print(filepath[len(filepath) - 1])
 
-        elif event.event_type == 'created':
-            # Take any action here when a file is first created.
-            print("Received created event - %s." % event.src_path)
-            filepath = event.src_path.split(os.sep)
-            sep = os.sep
-            print(filepath[len(filepath) - 1])
+      filename = filepath[len(filepath) - 1]
 
-            filename = filepath[len(filepath) - 1]
+      filepath.pop(len(filepath) - 1)
 
-            filepath.pop(len(filepath) - 1)
+      filepath = os.sep.join(filepath)
+      print(filepath)
 
-            filepath = os.sep.join(filepath)
-            print(filepath)
-
-            os.system(f"..\detection_scripts\ShapeRecognition.exe {filepath} {filename}")
+      os.system(f"..\detection_scripts\ShapeRecognition.exe {filepath} {filename}")
 
 if __name__ == "__main__":
     w = Watcher()
